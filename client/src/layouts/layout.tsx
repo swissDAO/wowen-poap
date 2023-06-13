1c9f776410ea3e93733fe7ab5017937a1657ec57
Olivier <olivier.winkler@icloud.com>
2023-06-13 13:55:43 +0200

    feat(layout): add gallery link

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -5,22 +5,18 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <nav className="block w-full px-8 shadow-md backdrop-saturate-200 backdrop-blur-2xl sticky top-0 z-10 py-5 lg:px-16 lg:py-7 max-w-full bg-wowen bg-opacity-100 text-rtm-white font-semibold rounded-none">
-        <div className="relative w-auto h-fit flex items-center">
+        <div className="relative w-auto h-fit flex items-center justify-between">
           <a className="hidden lg:block cursor-pointer active" href="/" aria-current="page">
             <img src="https://www.wowen.io/static/media/Wowen_white.bc50c8aa3083ed679764922c2aa5fc26.svg" className="h-fit" alt="Wowen Logo" />
           </a>
 
-          <div className="left-0">
-            <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg text-sm text-blue-500 active:bg-blue-500/30 mr-2 lg:hidden hover:bg-rtm-green-400" type="button">
-              <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-2xl text-rtm-green-300" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z">
-                  </path>
-                </svg>
-              </span>
-            </button>
-          </div>
-          <div className="flex items-center float-right ml-auto">
+          <ul className="h-fit mt-2 flex flex-col xl:gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center truncate">
+            <a className="rounded-lg py-2 px-2 2xl:px-4 font-sans text-white" href="/gallery">
+              Gallery
+            </a>
+          </ul>
+
+          <div className="flex items-center">
             <CustomConnectButton />
           </div>
         </div>
